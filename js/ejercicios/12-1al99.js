84ae7c49ed6618e471c425283da0a69816aac781
Nacho <barrojojoaquin2@gmail.com>
2025-04-21 15:09:30 -0300

    Un script que genere un número aleatorio entre 1 y 99

---
+++ js/ejercicios/12-1al99.js
@@ -0,0 +1,5 @@
+//Realiza un script que genere un número aleatorio entre 1 y 99
+
+const numero= Math.floor(Math.random() * 99) + 1;
+
+document.writeln("El número aleatorio es: " + numero + "<br>");
