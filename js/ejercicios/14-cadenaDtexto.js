bcd87715cbee940d2fb6305d7b4b49a593725fc8
Nacho <barrojojoaquin2@gmail.com>
2025-04-21 19:36:27 -0300

    un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter

---
+++ js/ejercicios/14-cadenaDtexto.js
@@ -0,0 +1,13 @@
+//Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
+
+let cadena = prompt("Introduce una cadena de texto: ");
+let resultado = "";
+
+for (let i = 0; i < cadena.length; i++) {
+    resultado += cadena[i];
+    if (i < cadena.length - 1) {
+        resultado += "-";
+    }
+}
+
+document.writeln(resultado);
