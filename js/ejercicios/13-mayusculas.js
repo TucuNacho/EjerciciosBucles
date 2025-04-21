9c15fbda244c145b866c487a0c8f6633669ae8a7
Nacho <barrojojoaquin2@gmail.com>
2025-04-21 15:19:16 -0300

    Un script que pide un texto y lo muestra en mayúsculas.

---
+++ js/ejercicios/13-mayusculas.js
@@ -0,0 +1,4 @@
+//- Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+const texto = prompt("Introduce un texto:").toUpperCase();
+document.writeln(`El texto en mayúsculas es: ${texto}`);
