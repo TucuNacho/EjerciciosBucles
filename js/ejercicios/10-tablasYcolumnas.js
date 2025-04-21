5c83530163873ced1c977fec2659ee77ddf56318
Nacho <barrojojoaquin2@gmail.com>
2025-04-21 14:43:10 -0300

    Un script que pide número de filas y columnas y escriba una tabla.

--- js/ejercicios/10-tablasYcolumnas.js
+++ js/ejercicios/10-tablasYcolumnas.js
@@ -1,5 +1,5 @@
-//Realiza un script que pida número de filas y columnas y escriba una tabla. 
-// Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. 
+//Realiza un script que pida número de filas y columnas y escriba una tabla.
+// Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente.
 // Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
 
 let filas = parseInt(prompt("Introduce el número de filas:"));
@@ -7,13 +7,13 @@ let columnas = parseInt(prompt("Introduce el número de columnas:"));
 let tabla = "<table border='1'>";
 let numero = filas * columnas;
 
-for (let i= 0; i < filas; i++){
-    tabla += "<tr>";
-    for (let j = 0; j < columnas; j++){
-        tabla += "<td>" + numero + "</td>";
-        numero--;
-    }
-    tabla += "</tr>";
+for (let i = 0; i < filas; i++) {
+  tabla += "<tr>";
+  for (let j = 0; j < columnas; j++) {
+    tabla += "<td>" + numero + "</td>";
+    numero--;
+  }
+  tabla += "</tr>";
 }
 
 tabla += "</table>";
