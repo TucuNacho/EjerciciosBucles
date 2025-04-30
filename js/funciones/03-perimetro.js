2fc961f0808b6e87bff2bcb57428e9d816c963d3
Nacho <barrojojoaquin2@gmail.com>
2025-04-30 17:58:31 -0300

    detalles corregidos en la funcion para calcular el perimetro

--- js/funciones/03-perimetro.js
+++ js/funciones/03-perimetro.js
@@ -2,7 +2,11 @@ const ladoRectangulo = parseInt(prompt("Ingrese el lado del rectángulo: "));
 const baseRectangulo = parseInt(prompt("Ingrese la base del rectángulo: "));
 
 const perimetro=(ladoRectangulo, baseRectangulo) => {
-    return (ladoRectangulo + baseRectangulo) * 2;
+    if (isNaN(ladoRectangulo) || isNaN(baseRectangulo)) {
+        return "Error: Ingrese un número válido.";
+    }else{
+        return (ladoRectangulo + baseRectangulo) * 2;
+    }
 };
 const resultadoPerimetro=perimetro(ladoRectangulo, baseRectangulo);
-document.writeln(`El perímetro del rectángulo es: ${perimetro(ladoRectangulo, baseRectangulo)}`);
+document.writeln(`El perímetro del rectángulo es: ${resultadoPerimetro}`);
