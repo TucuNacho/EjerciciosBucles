c6898d70c36e2386348a1be708137de8eff59c76
Nacho <barrojojoaquin2@gmail.com>
2025-05-02 16:17:03 -0300

    un pequeño error solucionado

--- js/07-objetos.js
+++ js/07-objetos.js
@@ -1,5 +1,4 @@
 //notacion literal - esto uso cuando voy a crear un objeto
-<<<<<<< HEAD
 const persona = {
   //propiedades o atributos
   //nombre, email, id,
@@ -41,37 +40,3 @@ document.writeln(`<p> el nuevo nombre de usuario es: ${persona.nombreUser} </p>`
 
 persona.nombreCompleto()
 persona.cambiarEstado()
-=======
-const usuario = {
-    //propiedades o atributos -> clave: valor
-    //nombreUsuario, nombre, Apellido, email, dni, id, github, telefono, fotoPerfil, estado
-    nombreUsuario: 'ignacio01',
-    nombre: 'Ignacio',
-    apellido: 'Barrojo',
-    email: 'ignacionbarrojo@mail.com',
-    dni: '42333444',
-    id: 23,
-    estado: true,
-    //metodos
-    nombreCompleto: function (){
-        document.writeln('Hola soy...')
-    },
-    cambiarEstado: ()=>{
-        document.writeln('Aqui tengo que cambiar el estado')
-    }
-}
-// mostrar un objeto
-console.log(usuario)
-document.writeln(usuario+'<br>')
-
-//mostrar propiedades de un objeto
-document.writeln(usuario.nombreUsuario)
-document.writeln(`<p>El dni del usuario es: ${usuario.dni}</p>`)
-document.writeln(`<p>El dni del usuario es: ${usuario.dnis}</p>`)
-document.writeln(`<p>El email del usuario es: ${ usuario['email'] }</p>`)
-document.writeln(`<p>El email del usuario es: ${ usuario['emails'] }</p>`)
-
-usuario.nombreCompleto()
-usuario.cambiarEstado()
-//clases
->>>>>>> upstream/poo
